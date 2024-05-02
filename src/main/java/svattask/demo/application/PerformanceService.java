74b74f7d136d8816d38b6282669d9d288c10eca1
Denys <denys.baranov@sixt.com>
2024-05-01 21:03:03 -0700

    initial

--- src/main/java/svattask/demo/application/PerformanceService.java
+++ src/main/java/svattask/demo/application/PerformanceService.java
@@ -6,40 +6,93 @@ import svattask.demo.dto.OverallComparisonDto;
 import svattask.demo.dto.PerformanceResultDto;
 import svattask.demo.enums.CrudMethods;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 public class PerformanceService {
 
     public PerformanceResultDto runPerformance(long listSize) {
-        List<MultiThreadLists> threads = new ArrayList<>();
-        // Run all threads
+        // Start CompletableFuture tasks asynchronously
+        List<CompletableFuture<Map<String, Map<CrudMethods, OperationMetricsDto>>>> threads = new ArrayList<>();
         PerformanceData.COLLECTION_TYPES
                 .forEach(collection -> {
-                    var listThread = new MultiThreadLists(collection, listSize);
-                    threads.add(listThread);
-                    listThread.start();
+                    var thread = collectStatistics(collection, listSize);
+                    threads.add(thread);
                 });
-        // Wait for all thread completion
-        threads.forEach(thread -> {
-            try {
-                thread.join();
-            }
-            catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        return mergeMeasuringResults(threads);
+        // Wait for both tasks to complete
+        CompletableFuture.allOf(threads.toArray(new CompletableFuture[0])).join();
+
+        // Return the results
+        return mergeMeasuringResults(threads.stream()
+                .map(x -> {
+                    try {
+                        return x.get();
+                    }
+                    catch (InterruptedException |
+                            ExecutionException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).toList()
+        );
     }
 
-    private PerformanceResultDto mergeMeasuringResults(List<MultiThreadLists> threads) {
-        var details = threads
-                .stream()
-                .map(MultiThreadLists::getDetails)
-                .toList();
+    private CompletableFuture<Map<String, Map<CrudMethods, OperationMetricsDto>>> collectStatistics(
+            List<String> list, long size
+    ) {
+        AtomicReference<List<String>> list2 = new AtomicReference<>(list);
+        Map<String, Map<CrudMethods, OperationMetricsDto>> details = new HashMap<>();
+        Map<CrudMethods, OperationMetricsDto> map = new HashMap<>();
+        return CompletableFuture.supplyAsync(() -> {
+            Arrays.stream(CrudMethods.values())
+                    .forEach(method -> {
+                        var operationMetrics = new OperationMetricsDto();
+                        for (int i = 0; i < size; i++) {
+                            Instant start = Instant.now();
+                            list2.set(listModification(list2.get(), method));
+                            Instant end = Instant.now();
+                            long executionTime = Duration.between(start, end).getNano();
+                            if (i == 0) {
+                                operationMetrics.setStart(executionTime);
+                            }
+                            else if (i == size / 2) {
+                                operationMetrics.setMiddle(executionTime);
+                            }
+                            else if (i == size - 1) {
+                                operationMetrics.setEnd(executionTime);
+                            }
+                        }
+
+                        map.put(method, operationMetrics);
+                        details.put(list.getClass().getSimpleName(), map);
+                    });
+            return details;
+        });
+
+    }
+
+    private List<String> listModification(List<String> list, CrudMethods crudMethod) {
+        switch (crudMethod) {
+            case CREATE -> list.addFirst("a");
+            case READ -> list.getFirst();
+            case UPDATE -> list.set(0, "b");
+            case DELETE -> list.removeLast();
+            default -> throw new RuntimeException("Method is not found");
+        }
+        return list;
+    }
+
+    private PerformanceResultDto mergeMeasuringResults(
+            List<Map<String, Map<CrudMethods, OperationMetricsDto>>> details
+    ) {
         Map<String, Long> averageMap = new HashMap<>();
         for (var detail : details) {
             String key = detail.keySet().iterator().next();
--- src/test/java/svattask/demo/DemoApplicationTests.java
+++ src/test/java/svattask/demo/DemoApplicationTests.java
@@ -1,8 +1,11 @@
 package svattask.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import svattask.demo.application.PerformanceService;
+
+import java.util.concurrent.ExecutionException;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -10,6 +13,15 @@ class DemoApplicationTests {
     @Test
     void contextLoads() {
         var re = new PerformanceService().runPerformance(100000);
+        System.out.println(re);
+    }
+
+    @Autowired
+    private PerformanceService performanceService;
+
+    @Test
+    void contextLoads2() throws ExecutionException, InterruptedException {
+        var re = performanceService.runPerformance(100000);
         System.out.println(re);
     }
 
