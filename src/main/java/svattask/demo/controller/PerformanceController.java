3d2c62c84f8559cd290604b687a4b1b19b729906
Denys <denys.baranov@sixt.com>
2024-05-09 14:56:37 -0700

    ResponseEntity

--- src/main/java/svattask/demo/controller/PerformanceController.java
+++ src/main/java/svattask/demo/controller/PerformanceController.java
@@ -1,6 +1,7 @@
 package svattask.demo.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,8 +17,8 @@ public class PerformanceController {
     private final PerformanceService performanceService;
 
     @GetMapping
-    public PerformanceResultDto runPerformance(@RequestParam long listSize) {
-        return performanceService.runPerformance(listSize);
+    public ResponseEntity<PerformanceResultDto> runPerformance(@RequestParam long listSize) {
+        return ResponseEntity.ok(performanceService.runPerformance(listSize));
     }
 
 }
